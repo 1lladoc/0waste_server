d80a461063112c5d18f771b3a2dd57985b45be3b
unolladoc <rubenlladoc@gmail.com>
2019-10-01 09:34:24 +0800

    upload server

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+include 'conn.php';
+header("Content-Type: application/json; charset=UTF-8");
+//$inputJSON = file_get_contents('php://input');
+$inputJSON = '{
+    "email": "admin@menro.com",
+    "password": "admin123"
+}';
+
+$input = json_decode($inputJSON, FALSE); //convert JSON into array
+
+$output = array();
+
+$stmt = $conn->prepare("SELECT id, TYPE FROM users WHERE email = ? AND password = MD5(?);");
+$stmt->bind_param("ss", $input->email, $input->password);
+$stmt->execute();
+$stmt->bind_result($id, $type);
+$tmp = array();
+while ($stmt->fetch()) {
+    $tmp["id"] = $id;
+    $tmp["type"] = $type;
+    array_push($output, $tmp);
+}
+
+$json = json_encode($output);
+echo $json;
+?>
