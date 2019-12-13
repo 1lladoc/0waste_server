1fd7dd92943cb6f410c966fb9091f646860d4b84
CherrySorio25 <jonaramoran19@gmail.com>
2019-12-13 13:50:45 +0800

    added datenow for online server fix

--- conn.php
+++ conn.php
@@ -6,6 +6,9 @@ define('DB_DATABASE', "zerowaste"); // database name
 define('DB_SERVER', "localhost"); // db server
  
 $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+date_default_timezone_set('Asia/Manila');
+$datenow = date('Y-m-d H:i:s');
  
 // Check connection
 if(mysqli_connect_errno())
