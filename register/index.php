7ad8fd23303393c74b76d2b1d870dcc8c05493d3
CherrySorio25 <jonaramoran19@gmail.com>
2019-12-13 10:36:31 +0800

    added email and mobile-exist

--- register/index.php
+++ register/index.php
@@ -5,29 +5,60 @@ header("Content-Type: application/json; charset=UTF-8");
 include '../conn.php';
 
 $inputJSON = file_get_contents('php://input');
-$input = json_decode($inputJSON);
+
+// $inputJSON = '
+//     {
+//         "email": "admin1@menro.com",
+//         "password": "12345678",
+//         "name": "r",
+//         "number": "11111111112",
+//         "usertype": 1,
+//         "latitude": 10.5,
+//         "longitude": 10.5
+//     }
+// ';
+
+$input = json_decode($inputJSON, false);
 
 $output = array();
 
-$stmt = $conn->prepare("INSERT INTO users VALUES(NULL,?,MD5(?),?,?,?,?,?);");
-$stmt->bind_param("ssssidd",
-    $input->email,
-    $input->password,
-    $input->name,
-    $input->number,
-    $input->usertype,
-    $input->latitude,
-    $input->longitude
-);
+$stmtnumber = $conn->prepare("SELECT mobile FROM users WHERE mobile = ?");
+$stmtnumber->bind_param("s",$input->number);
+$stmtnumber->execute();
+$mobilen = $stmtnumber->fetch();
+$stmtnumber->close();
 
-if(!$stmt->execute()){
-    $output = array("status" => 69, "message" => "Error Inserting New Record");
+$stmtemail = $conn->prepare("SELECT users.email FROM users WHERE users.email = ?");
+$stmtemail->bind_param("s",$input->email);
+$stmtemail->execute();
+$emailn = $stmtemail->fetch();
+$stmtemail->close();
+
+if($mobilen){
+    $output = array("status" => 67, "message" => "Mobile Number already exist!");
+}else if($emailn){
+    $output = array("status" => 68, "message" => "Email Address already exist!");
 }else{
-    $output = array("status" => 70, "message" => "Registered Successfully");
+    $stmt = $conn->prepare("INSERT INTO users VALUES(NULL,?,MD5(?),?,?,?,?,?);");
+    $stmt->bind_param("ssssidd",
+        $input->email,
+        $input->password,
+        $input->name,
+        $input->number,
+        $input->usertype,
+        $input->latitude,
+        $input->longitude
+    );
+    if(!$stmt->execute()){
+        $output = array("status" => 69, "message" => "Error Inserting New Record");
+        echo($stmt->error);
+    }else{
+        $output = array("status" => 200, "message" => "Registered Successfully");
+    }
+    //$output = array("status" => 200, "message" => "Registered Successfully");
+    $stmt->close();
 }
 
 echo json_encode($output);
-
-$stmt->close();
 $conn->close();
 ?>
